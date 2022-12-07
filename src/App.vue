385b9323eb10717be25a332a7f2e50c26beed0b2
shijizhe <shijizhe@inspur.com>
2022-12-07 14:50:52 +0800

    引入 iview

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainTest msg="Father"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainTest from './components/MainTest.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainTest
   }
 }
 </script>
